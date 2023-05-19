fbba10dcf2b8ae9a3df610e91a452cb7e270673b
JW Park <117083314+jwpggit@users.noreply.github.com>
2023-05-20 00:00:10 +0900

    Delete routes directory

--- routes/email.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const Router = express.Router();
-const emailController = require('../controller/emailController');
-const uploadImage = require('../utils/multer');
-
-Router.post('/', uploadImage.single('image'), emailController.create);
-
-module.exports = Router
--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const emailRouter = require('./email')
-
-// Posts
-router.use('/email', emailRouter)
-
-module.exports = router
